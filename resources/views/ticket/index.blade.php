da7afbf73ea4a2fc5973106e587377784ade29e9
likhaya-coder <llikhaya.kulati@gmail.com>
2024-12-18 15:25:29 +0200

    changed width to full width

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -2,20 +2,18 @@
     <div class="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900">
         <div class="w-full sm:max-w-2xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             <h1 class="text-6xl font-semibold mb-4 text-white text-center">List of Posts</h1>
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                @foreach ($tickets as $ticket)
-                    <div class="bg-white p-4 shadow-md rounded-md">
-                        <h2 class="text-lg font-semibold">{{ $ticket->title }}</h2>
-                        <p class="text-gray-600">{{ $ticket->description }}</p>
-                        <form action="{{route('ticket.show', $ticket->id)}}">
-                            @csrf
-                            <x-primary-button class="mt-6">
-                                {{ __('Solve ticket') }}
-                            </x-primary-button>
-                        </form>
-                    </div>
-                @endforeach
-            </div>
+            @foreach ($tickets as $ticket)
+                <div class="w-full bg-white p-4 shadow-md rounded-md">
+                    <h2 class="text-lg font-semibold">{{ $ticket->title }}</h2>
+                    <p class="text-gray-600">{{ $ticket->description }}</p>
+                    <form action="{{route('ticket.show', $ticket->id)}}">
+                        @csrf
+                        <x-primary-button class="mt-6">
+                            {{ __('Solve ticket') }}
+                        </x-primary-button>
+                    </form>
+                </div>
+            @endforeach
         </div>
     </div>
 </x-app-layout>
